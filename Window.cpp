c0002edc9559a68a88df5ed16362c8375eaf5c93
Kyung yul Kevin Lim <thekoalaz@gmail.com>
2016-04-09 03:39:43 -0700

    Initial add.

---
+++ Window.cpp
@@ -0,0 +1,122 @@
+#include "window.h"
+
+const char* window_title = "GLFW Starter Project";
+Cube * cube;
+GLint shaderProgram;
+
+// Default camera parameters
+glm::vec3 cam_pos(0.0f, 0.0f, 20.0f);		// e  | Position of camera
+glm::vec3 cam_look_at(0.0f, 0.0f, 0.0f);	// d  | This is where the camera looks at
+glm::vec3 cam_up(0.0f, 1.0f, 0.0f);			// up | What orientation "up" is
+
+int Window::width;
+int Window::height;
+
+glm::mat4 Window::P;
+glm::mat4 Window::V;
+
+void Window::initialize_objects()
+{
+	cube = new Cube();
+
+	// Load the shader program. Similar to the .obj objects, different platforms expect a different directory for files
+#ifdef _WIN32 // Windows (both 32 and 64 bit versions)
+	shaderProgram = LoadShaders("../shader.vert", "../shader.frag");
+#else // Not windows
+	shaderProgram = LoadShaders("shader.vert", "shader.frag");
+#endif
+}
+
+void Window::clean_up()
+{
+	delete(cube);
+	glDeleteProgram(shaderProgram);
+}
+
+GLFWwindow* Window::create_window(int width, int height)
+{
+	// Initialize GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return NULL;
+	}
+
+	// 4x antialiasing
+	glfwWindowHint(GLFW_SAMPLES, 4);
+
+	// Create the GLFW window
+	GLFWwindow* window = glfwCreateWindow(width, height, window_title, NULL, NULL);
+
+	// Check if the window could not be created
+	if (!window)
+	{
+		fprintf(stderr, "Failed to open GLFW window.\n");
+		glfwTerminate();
+		return NULL;
+	}
+
+	// Make the context of the window
+	glfwMakeContextCurrent(window);
+
+	// Set swap interval to 1
+	glfwSwapInterval(1);
+
+	// Get the width and height of the framebuffer to properly resize the window
+	glfwGetFramebufferSize(window, &width, &height);
+	// Call the resize callback to make sure things get drawn immediately
+	Window::resize_callback(window, width, height);
+
+	return window;
+}
+
+void Window::resize_callback(GLFWwindow* window, int width, int height)
+{
+	Window::width = width;
+	Window::height = height;
+	// Set the viewport size
+	glViewport(0, 0, width, height);
+
+	if (height > 0)
+	{
+		P = glm::perspective(45.0f, (float)width / (float)height, 0.1f, 1000.0f);
+		V = glm::lookAt(cam_pos, cam_look_at, cam_up);
+	}
+}
+
+void Window::idle_callback()
+{
+	// Call the update function the cube
+	cube->update();
+}
+
+void Window::display_callback(GLFWwindow* window)
+{
+	// Clear the color and depth buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Use the shader of programID
+	glUseProgram(shaderProgram);
+	
+	// Render the cube
+	cube->draw(shaderProgram);
+
+	// Gets events, including input such as keyboard and mouse or window resizing
+	glfwPollEvents();
+	// Swap buffers
+	glfwSwapBuffers(window);
+}
+
+void Window::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Check for a key press
+	if (action == GLFW_PRESS)
+	{
+		// Check if escape was pressed
+		if (key == GLFW_KEY_ESCAPE)
+		{
+			// Close the window. This causes the program to also terminate.
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+	}
+}
